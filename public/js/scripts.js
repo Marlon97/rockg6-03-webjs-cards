f528f77b660f4744116decdbd7f81c3f81558bbb
Marlon97 <A01379404@itesm.mx>
2021-06-16 16:11:45 -0500

    separación de cosas a sus respectivo documentos

---
+++ public/js/scripts.js
@@ -0,0 +1,46 @@
+const createCard = (number, symbol, isNumber) => {
+    const cardDiv = document.createElement('div');
+
+    cardDiv.classList.add('card');
+    cardDiv.setAttribute('symbol',symbol);
+    cardDiv.setAttribute('number', number);
+
+    cardDiv.innerHTML = `
+            <div class="card-corner top-left">
+              <div>${number}</div>
+              <div>${symbol}</div>
+            </div>
+            <div class="symbols">
+              ${(isNumber) ? `${new Array(parseInt(number))
+        .fill(symbol)
+        .map((cardSymbol) => `
+                    <div>${cardSymbol}</div>
+                `)
+        .join('')
+    }` : ''}
+            </div>
+            <div class="card-corner bottom-right">
+              <div>${number}</div>
+              <div>${symbol}</div>
+            </div>
+          `;
+
+    return cardDiv;
+}
+
+(async () => {
+    const deck = await  (await fetch('/deck')).json();
+    const container = document.querySelector('.deck');
+
+
+
+    deck.forEach((card) => {
+        const number = card.slice(0, -1);
+        const symbol = card.slice(-1);
+        const isNumber = !isNaN(number);
+
+        container.append(createCard(number, symbol, isNumber));
+
+    });
+
+})();
--- server.js
+++ server.js
@@ -7,46 +7,8 @@ const { Deck, Hand } = require('./app/deck/deck');
 
 const deck = new Deck();
 
-app.get('/', (req, res) => {
-    res.send(`<html>
-    <head>
-      <title>Deck.js</title>
-      <link rel="stylesheet" href="./Styles/main.css">
-    </head>
-    <body>
-      <h1>Deck.js</h1>
-
-      <div class="deck">${deck.cards
-        .map((card) => {
-            const number = card.slice(0, -1);
-            const symbol = card.slice(-1);
-            const isNumber = !isNaN(number);
-            return `<div class="card" symbol="${symbol}" number="${number}">
-            <div class="card-corner top-left">
-              <div>${number}</div>
-              <div>${symbol}</div>
-            </div>
-            <div class="symbols">
-              ${(isNumber) ? `${new Array(parseInt(number))
-                .fill(symbol)
-                .map((cardSymbol) => `
-                  <div>${cardSymbol}</div>
-                `)
-                .join('')
-            }` : ''}
-            </div>
-            <div class="card-corner bottom-right">
-              <div>${number}</div>
-              <div>${symbol}</div>
-            </div>
-          </div>`
-        })
-        .join('')
-    }</div>
-
-    </body>
-
-  </html>`);
+app.get('/deck', (req,res) => {
+   res.send(deck.cards);
 });
 
 app.listen(4001, () => {
