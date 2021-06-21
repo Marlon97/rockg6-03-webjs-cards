0da059b02c9fcc654f226a940bf7100ba5b98c59
Marlon97 <A01379404@itesm.mx>
2021-06-21 16:48:18 -0500

    cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.use(express.static('public'));
+
+app.use(cors());
 
 const { Deck, Hand } = require('./app/deck/deck');
 
